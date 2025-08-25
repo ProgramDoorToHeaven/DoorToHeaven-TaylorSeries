b4342ff587674f7b8420410afb96bbef124e2f83
McLayn <mclayn.v@gmail.com>
2025-08-25 19:29:48 +0200

    html lang

--- files/tools/html_builder.py
+++ files/tools/html_builder.py
@@ -39,7 +39,7 @@ def html_to_string(tag: HtmlTag, omit_doctype: bool = False) -> str:
 
 
 def build_html(main_div: HtmlTag) -> HtmlTag:
-    html_doc = new_tag("html")
+    html_doc = new_tag("html", lang="en")
     html_head = new_tag("head")
     html_head.append(new_tag("link", rel="stylesheet", href="../tools/main.css"))
     html_head.append(new_tag("link", rel="shortcut icon", type="image/png", href="../tools/favicon.png"))
