9831dee1981e1c8d6583856abf9e0afade9e7127
McLayn <mclayn.v@gmail.com>
2025-08-30 21:42:55 +0200

    money in Cairo

--- files/1994/19940824_19360000_Cairo.py
+++ files/1994/19940824_19360000_Cairo.py
@@ -2,4 +2,5 @@
 It's been over 5 years and still no success deciphering.
 The ring is precious.
 The hieroglyphs say translate to...
+Money running out. Translated a Hebrew amulet for a French colleague. Germans started obsessing about various artefacts with https://en.wikipedia.org/wiki/Ahnenerbe, so they might help financing our research.
 """
