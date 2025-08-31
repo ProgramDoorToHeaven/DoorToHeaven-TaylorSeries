feb8410bad7c8a29e3d4c070196bfd3f4b3d1319
McLaynV <25386191+McLaynV@users.noreply.github.com>
2025-08-31 09:36:32 +0200

    Update 19940824_19360000_Cairo.py

--- files/1994/19940824_19360000_Cairo.py
+++ files/1994/19940824_19360000_Cairo.py
@@ -23,4 +23,17 @@ With my daughter Catherine, we've been living in the warehouse where we have all
 She even got her first part time job - cataloging items at the Cairo museum - to help with our financial situation. 
 I've also been doing some odd jobs to get us more money. 
 Like last week, I translate some Hebrew inscriptions for some French archeologist.
+
+I don't want to lose the opportunity to work on this ring.
+It is my life's work. 
+It is precious to me.
+And that's why I'm writing to you.
+You mentioned that the German government recently established the Ahnenerbe project and they are interested in various ancient artifacts. 
+Could you please try to get some funding from them? 
+I don't have any contacts in Germany anymore.
+That is besides you.
+If you help with the paperwork, you can have your share.
+
+With kindest regards,
+Paul Langford
 """
