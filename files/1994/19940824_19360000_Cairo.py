a309737cfe404d1cd920b162ff0a80a2c1313269
McLaynV <25386191+McLaynV@users.noreply.github.com>
2025-08-31 09:25:28 +0200

    Update 19940824_19360000_Cairo.py

--- files/1994/19940824_19360000_Cairo.py
+++ files/1994/19940824_19360000_Cairo.py
@@ -6,4 +6,21 @@ The ring is precious.
 The hieroglyphs say translate to...
 Professor Langford had a team of archaeologists and linguists translate the coverstone. They used Budge as a reference and translated the name of the device as "Doorway to Heaven".
 Money running out. Translated a Hebrew amulet for a French colleague. Germans started obsessing about various artefacts with https://en.wikipedia.org/wiki/Ahnenerbe, so they might help financing our research.
+
+
+Dear Deitrich,
+
+I've been working on the ancient ring for over 5 years and it feels like I haven't moved a bit. 
+The only success since it's discovery has been the translation of the hieroglyphs on the cover stone.
+
+... HIEROGLYPHS
+... TRANSLATION 
+
+I can feel that it is an important discovery, but without any successful research, it is just an "unknown artifact", a curiosity to hang on a wall and wonder about.
+Moreover, it is getting difficult to do any research with expensive equipment or with renowned colleagues when our finding is running dry. 
+
+With my daughter Catherine, we've been living in the warehouse where we have all the artifacts. 
+She even got her first part time job - cataloging items at the Cairo museum - to help with our financial situation. 
+I've also been doing some odd jobs to get us more money. 
+Like last week, I translate some Hebrew inscriptions for some French archeologist.
 """
