5b1c523928f6f0365b48506aa0088d0cd4d74b2a
McLaynV <25386191+McLaynV@users.noreply.github.com>
2025-08-31 09:37:45 +0200

    Update 19940824_19360000_Cairo.py

--- files/1994/19940824_19360000_Cairo.py
+++ files/1994/19940824_19360000_Cairo.py
@@ -1,13 +1,4 @@
 """
-A letter to Deitrich:
-
-It's been over 5 years and still no success deciphering.
-The ring is precious.
-The hieroglyphs say translate to...
-Professor Langford had a team of archaeologists and linguists translate the coverstone. They used Budge as a reference and translated the name of the device as "Doorway to Heaven".
-Money running out. Translated a Hebrew amulet for a French colleague. Germans started obsessing about various artefacts with https://en.wikipedia.org/wiki/Ahnenerbe, so they might help financing our research.
-
-
 Dear Deitrich,
 
 I've been working on the ancient ring for over 5 years and it feels like I haven't moved a bit. 
