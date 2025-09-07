3273dc60d331894d6adbaed44713d08de3b875a3
McLayn <mclayn.v@gmail.com>
2025-09-07 14:57:03 +0200

    telegram format

--- files/1994/19940824_19390822_Telegram.py
+++ files/1994/19940824_19390822_Telegram.py
@@ -21,33 +21,31 @@ That's a week before the war started.
 from tools import *
 
 PAGE = Page(blocks=(
-    PageBlockTypeWriter(break_lines=52, parts=(
-        Paragraph(text=("TRANSCONTINENTAL COMMUNICATION COMPANY, LTD.",)),
-        Paragraph(is_literal=True, text=("""
-            81236      Words     BERLIN N°1    CAIRO, EGYPT
-            8 22 39    Charge    AUG 22 '39    W.F.GALTZ
+    # SGO:Catherine 00:03:16
+    PageBlockTypeWriter(parts=(
+        LiteralParagraph(strip_spaces_from_line_starts_according_to_line=3, text=("""
+              TRANSCONTINENTAL COMMUNICATION COMPANY, LTD.
+
+            81236      Words      BERLIN N°1    CAIRO, EGYPT
+            8 22 39    Charge     AUG 22 '39    W.F.GALTZ
             18:30      7 Ø DM
-        """,)),
-        HorizontalLine(omit_space_before=True),
-        Paragraph(omit_space_before=True, keep_spaces=True, text=(
-            "TO Receiver's Name:  DOCTOR PAUL LANGFORD",
-        )),
-        Paragraph(omit_space_before=True, keep_spaces=True, text=(
-            "           Address: WAREHOUSE DIST.No5 CAIRO, EGYPT",
-        )),
-        Paragraph(text=("THIS TELEGRAM WILL BE CHARGED AT FULL RATE",)),
-        HorizontalLine(omit_space_before=True),
-        Paragraph(omit_space_before=True, text=("Doctor Langford --",)),
-        Paragraph(is_literal=True, text=("""
+            ----------------------------------------------------
+            TO Receiver's Name:  DOCTOR PAUL LANGFORD
+                       Address: WAREHOUSE DIST.No5 CAIRO, EGYPT
+
+            THIS TELEGRAM WILL BE CHARGED AT FULL RATE
+            ----------------------------------------------------
+            Doctor Langford --
+
             I regret to inform that our contacts in
             Germany have not come through as
             we had hoped.
-        """,)),
-        Paragraph(is_literal=True, text=("""
+
             This will be my last missive.
             Goodbye dear friend.
+
+                                    -- Deitrich
         """,)),
-        Paragraph(keep_spaces=True, text=("                     -- Deitrich",)),
     )),
 ))
 
