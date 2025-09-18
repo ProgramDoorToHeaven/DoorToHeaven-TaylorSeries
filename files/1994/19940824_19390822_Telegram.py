476070ee1f4a788204afb7ba937eb7b3f25cf4aa
McLayn <mclayn.v@gmail.com>
2025-09-18 21:42:57 +0200

    Telegram - strange message

--- files/1994/19940824_19390822_Telegram.py
+++ files/1994/19940824_19390822_Telegram.py
@@ -21,6 +21,18 @@ That's a week before the war started.
 from tools import *
 
 PAGE = Page(blocks=(
+    PageBlock(parts=(
+        Paragraph(text=("""
+            A telegram...
+            I’ve rarely encountered telegrams in my life.
+            By around 1980 (when I went to primary school), telegrams had begun to fade as fax machines took over, with both methods seeing roughly equal use in that period.
+            Soon after, fax machines were themselves overtaken by the rapid rise of internet-based e-mail.
+            Today, while fax still exists and some companies still provide telegram services, e-mail has become the dominant form of written communication.
+        """,)),
+        Paragraph(text=("""
+            But during that time, telegrams were a common and reliable way to send urgent messages.
+        """,)),
+    )),
     # SGO:Catherine 00:03:16
     PageBlockTypeWriter(parts=(
         LiteralParagraph(strip_spaces_from_line_starts_according_to_line=3, text=("""
@@ -49,15 +61,12 @@ PAGE = Page(blocks=(
     )),
     PageBlock(parts=(
         Paragraph(text=("""
-            This was just a week before the outbreak of World War II,
-            though the war itself didn’t reach Egypt until two years later, in 1940.
-            During that time, telegrams were a common and reliable way to send urgent messages.
+            That is a strange message, considering they had been working together for years.
+            I wonder what happened to Deitrich in Berlin.
         """,)),
         Paragraph(text=("""
-            I’ve rarely encountered telegrams in my life.
-            By around 1980 (when I went to primary school), telegrams had begun to fade as fax machines took over, with both methods seeing roughly equal use in that period.
-            Soon after, fax machines were themselves overtaken by the rapid rise of internet-based e-mail.
-            Today, while fax still exists and some companies still provide telegram services, e-mail has become the dominant form of written communication.
+            This was just a week before the outbreak of World War II,
+            though the war itself didn’t reach Egypt until two years later, in 1940.
         """,)),
     )),
 ))
