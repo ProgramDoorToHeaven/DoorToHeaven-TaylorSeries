05f12b4f4f9595995ba51e4d18501e54b418ab9c
Vojtěch Miklín <miklin@sefira.cz>
2026-02-17 18:59:33 +0100

    Achilles

--- docs/1994/19940824_19390900_Achilles.py
+++
@@ -1,7 +0,0 @@
-"""
-Captain Mitchell, Merchant Marine
--> Boston, zigzagging all over the Atlantic to make it difficult to U-boats
-"We're not at war yet" = USA?
-
-"torpedo" hole, grandson Cameron Mitchell
-"""
---
+++ docs/1994/19940824_19390900_Achilles/__init__.py
@@ -0,0 +1,24 @@
+"""
+Captain Mitchell, United States Merchant Marine, Achilles ship
+-> Boston, zigzagging all over the Atlantic to make it difficult to U-boats
+"torpedo" hole, grandson Cameron Mitchell
+
+Departed 1939-08-25 -ish.
+
+In early 1930s, from Southampton to New York (5'700 km), it took:
+    5-7 days for express passenger lines
+    7-9 days for regular passenger lines
+    10-12 days for cargo and slower ships
+In 1939, the Achilles journey from Alexandria to Boston (9'000 km) would be +60% distance going straight.
+With zig-zagging through the Atlantic, let's say (10'000 km) +75% distance compared to Southampton -> New York.
+We can expect faster ships at the end of 1930s.
+So let's say it would be 12-14 days for Achilles going straight and 14-16 days when zig-zagging through the Atlantic.
+
+"We'd be in Boston right now if we weren't zigzagging all over the bloody Atlantic."
+That means the Baal boarding hapens near to the end of the journey - arround day 12-14 with 2 days remaining.
+There could be some exageration in the statement, so maybo arround day 9-14 with 2-5 days remaining.
+That would be 1939-09-03 to 1939-09-08.
+
+France + UK declared war on Germany on 1939-09-03
+"We're not at war yet" (= USA entered WW2 in 1941)
+"""
