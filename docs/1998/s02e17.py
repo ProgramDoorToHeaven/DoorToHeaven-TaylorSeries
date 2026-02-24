e42e8268e3b236efb9c572e6db9e1080daf6e605
McLayn <mclayn.v@gmail.com>
2026-02-24 22:26:33 +0100

    Update s02e17.py

--- docs/1998/s02e17.py
+++ docs/1998/s02e17.py
@@ -1,3 +1,40 @@
 """
-meditation, brain waves
+Keep some of the topics for s03e20 and other ascension episodes.
+
+meditation, brain waves, zen
+
+The meaning of one's life:
+You have to find it on your own.
+Asking others to help you find the meaning of life is like wanting them to eat when you are the one who is hungry.
+It is about your own internal experience.
+
+Masters of zen don't want to explain to you how water tastes. They want you to drink it.
+
+Instead of words and phrases, express yourself.
+
+Word is a finger pointing towards the Moon.
+Do not confuse the finger with the Moon.
+The language can only point towards an experience - it can't transfer an experience.
+
+The more you try to relax, the less relaxed you are.
+
+Meditation:
+Passive part:
+Do not continue the train of thoughts, but do not supress it.
+Resign to your self, goals, and efforts.
+Calm and open your mind.
+Active part:
+Vigilant attention to what is currently happening inside.
+Think about your own thoughts dispassionately, with distance.
+Living in the present moment.
+
+Even if you meditate the same way Buddha did, you will not become Buddha.
+If your cart stops, do not whip the cart, whip the draft animal.
+
+Ten people swam over a river.
+When they wanted to count themselves on the other side, each of them only counted nine.
+Each forgot about themselves.
+Don't forget that you are part of the world, and you can only experience it through your own mind.
+
+Who are you? Where do you come from? Where are you heading?
 """
